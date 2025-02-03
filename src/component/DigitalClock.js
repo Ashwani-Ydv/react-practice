755e38d1af5e3dbe498e8185e24580117174a7b7
Ashwani Yadav <yashwani1021@gmail.com>
2025-02-03 17:46:06 +0530

    stop watch

--- src/component/DigitalClock.js
+++ src/component/DigitalClock.js
@@ -1,36 +1,60 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const DigitalClock = () => {
-    const [hour,setHour] = useState(0);
-    const [minutes,setMinutes] = useState(0);
-    const [seconds,setSeconds] = useState(0);
+    const [hour, setHour] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [isRunning,setIsRunning]=useState(false);
 
-    useEffect(()=>{
-        const intervalID= setInterval(()=>{
-            setSeconds(seconds+1);
-            if(seconds>=59){
-                setMinutes(minutes+1)
-                setSeconds(0)
+    useEffect(() => {
+        if(isRunning){
+            const intervalID = setInterval(() => {
+                setSeconds(seconds + 1);
+                if (seconds >= 59) {
+                    setMinutes(minutes + 1)
+                    setSeconds(0)
+                }
+                if (minutes >= 59) {
+                    setHour(hour + 1);
+                    setMinutes(0);
+                }
+            }, 1000)
+            return () => {
+                clearInterval(intervalID);
             }
-            if(minutes>=59){
-                setHour(hour+1);
-                setMinutes(0);
-            }
-        },1000)
-       return ()=>{
-        clearInterval(intervalID);
-       }
+        }
     })
-    
-    
-    return(
+
+    const handleReset=()=>{
+        setHour(0);
+        setMinutes(0);
+        setSeconds(0);
+        setIsRunning(false);
+    }
+
+    const handleStart=()=>{
+     setIsRunning(true);
+    }
+
+    const handleStop=()=>{
+        setIsRunning(false)
+
+    }
+
+
+    return (
         <div>
             <h1>Digital Clock</h1>
-            {hour<10?`0${hour}`:hour}:{minutes<10?`0${minutes}`:minutes}:{seconds<10?`0${seconds}`:seconds}
+            <div>
+                <button onClick={handleReset}>Reset</button>
+                <button onClick={handleStart}>Start</button>
+                <button onClick={handleStop}>Stop</button>
+            </div>
+            {hour < 10 ? `0${hour}` : hour}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
         </div>
     )
-    
-    }
+
+}
 
 
 export default DigitalClock;
